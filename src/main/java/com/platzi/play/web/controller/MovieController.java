51667e82a3f7a011647716007fc2a1b110be179b
romyluna <134311514+romyluna@users.noreply.github.com>
2025-10-22 16:25:32 -0300

    feat: agrego el endpoint para que traiga todas las peliculas

--- src/main/java/com/platzi/play/web/controller/MovieController.java
+++ src/main/java/com/platzi/play/web/controller/MovieController.java
@@ -1,4 +1,28 @@
 package com.platzi.play.web.controller;
 
+import com.platzi.play.persistence.crud.CrudMovieEntity;
+import com.platzi.play.persistence.entity.MovieEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class MovieController {
+
+    private final CrudMovieEntity crudMovieEntity;
+
+    public MovieController(CrudMovieEntity crudMovieEntity) {
+        this.crudMovieEntity = crudMovieEntity;
+    }
+
+   /*http://localhost:8090/platzi-play/api/movies*/
+
+    @GetMapping("/movies")
+    public List<MovieEntity> getAll(){
+        return (List<MovieEntity>) this.crudMovieEntity.findAll(); /*casteo para que devuelva una lista*/
+    }
+
+
+
 }
